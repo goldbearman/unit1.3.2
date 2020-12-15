a665ec6694e59212bc0de2b4ac8e2bd60f01428e
goldbearman <sabirovfarit@google.com>
2020-12-15 23:28:56 +0300

    swiper all

--- slider.js
+++ slider.js
@@ -1,25 +1,35 @@
-new Swiper('.image-slider',{
-
-    // If we need pagination
-    pagination: {
-        el: '.swiper-pagination',
-        clickable: true,
-    },
-    keyboard:{
-        //Включаем упрвление калвиатурой
-        enabled: true,
-        //Только когда слайдер в пределах вьюпорта
-        onlyInViewport: true,
-    },
-
-    mousewheel:{
-      sensitivity:1,
-    },
-
-    grabCursor: true,
-    slideToClickedSlide:true,
-
-    slidesPerView: 'auto',
-
-
+const slider = document.querySelector('.swiper-container');
+let mySwiper;
+function mobileSlider() {
+    if (window.innerWidth <= 768 && slider.dataset.mobile == 'false') {
+        mySwiper = new Swiper('.image-slider', {
+            // If we need pagination
+            pagination: {
+                el: '.swiper-pagination',
+                clickable: true,
+            },
+            keyboard: {
+                //Включаем упрвление калвиатурой
+                enabled: true,
+                //Только когда слайдер в пределах вьюпорта
+                onlyInViewport: true,
+            },
+            mousewheel: {
+                sensitivity: 1,
+            },
+            grabCursor: true,
+            slideToClickedSlide: true,
+            slidesPerView: 'auto',
+        });
+        slider.dataset.mobile = 'true';
+    }
+    if (window.innerWidth > 768) {
+        slider.dataset.mobile = 'false';
+        mySwiper.destroy();
+    }
+}
+mobileSlider();
+window.addEventListener('resize', () => {
+    mobileSlider();
 });
+
