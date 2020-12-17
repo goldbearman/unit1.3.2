8b4068c24d5ffcc73f010848bc48c28041b689f4
fsabirov <sabirovfarit@gmail.com>
2020-12-17 16:30:07 +0300

    Исправления по макету 1.5

--- slider.js
+++ slider.js
@@ -38,3 +38,13 @@ window.addEventListener('resize', () => {
     mobileSlider();
 });
 
+var makeElement = function (tagName, className, text) {
+    var element = document.createElement(tagName);
+    element.classList.add(className);
+    if (text) {
+        element.textContent = text;
+    }
+    return element;
+};
+
+
