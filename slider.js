edf9c0b4509cc85c8d0720add89483c58b962981
goldbearman <sabirovfarit@google.com>
2020-12-21 08:02:20 +0300

    переделал бэк кнопок

--- slider.js
+++ slider.js
@@ -38,13 +38,13 @@ window.addEventListener('resize', () => {
     mobileSlider();
 });
 
-var makeElement = function (tagName, className, text) {
-    var element = document.createElement(tagName);
-    element.classList.add(className);
-    if (text) {
-        element.textContent = text;
-    }
-    return element;
-};
+// var makeElement = function (tagName, className, text) {
+//     var element = document.createElement(tagName);
+//     element.classList.add(className);
+//     if (text) {
+//         element.textContent = text;
+//     }
+//     return element;
+// };
 
 
